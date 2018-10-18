fa03c770cd337114d6348cd312a6ade4447b0b8e
elGnomo <efrainantonio@ideashappy.com>
2018-10-18 16:49:49 -0500

    while

---
+++ 04_whiMyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+// While
+var i = 1 //contador
+while i <= 100{//condicion
+    print(i)
+    i = i + 1
+}
+
+//Repeat
+//var i = 1 inicio
+//repeat{
+//    print(i)
+//    i = i + 1
+//} while i <= 100 //condicion
