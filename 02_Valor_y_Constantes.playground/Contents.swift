0a65dd15ab45b756b5aba595e37b45c12ae600e1
elGnomo <efrainantonio@ideashappy.com>
2018-10-18 16:18:19 -0500

    Introduccion a swift variables

---
+++ 02_Valor_y_Constantes.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+//var str : Tipo = "value"
+
+var str : String = "Esto es un texto"
+str = "nuevo texto"
+
+let constante : String = "Soy una constante"
+
+var precio : Int = 300
+precio = precio + 100
+
+let gravedad = 9.81
+
+
