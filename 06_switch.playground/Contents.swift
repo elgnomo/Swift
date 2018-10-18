0ff38ce1625de5903835be881f58c03ea71d398d
elGnomo <efrainantonio@ideashappy.com>
2018-10-18 17:02:50 -0500

    switch

---
+++ 06_switch.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+let diaDelaSemana = "fin de semana"
+switch diaDelaSemana {
+case "jueves":
+    print ("hoy es jueves")
+case "martes":
+    print ("hoy es martes")
+default:
+    print("hoy es un dia valido")
+}
