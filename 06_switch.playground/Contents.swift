3a0c0bf256e89a64d4ca7a81c7b2a2bec919ef30
elGnomo <efrainantonio@ideashappy.com>
2018-10-18 17:05:49 -0500

    switch

--- 06_switch.playground/Contents.swift
+++ 06_switch.playground/Contents.swift
@@ -9,3 +9,19 @@ case "martes":
 default:
     print("hoy es un dia valido")
 }
+
+let speed = 10
+
+//con rangos
+switch speed {
+    case 0...20:
+    print("Vas demasiado lento")
+    case 21...60:
+    print("Vas a una velocidad aceptable")
+    case 61...100:
+    print("Vas demasiado rápido")
+default:
+    print("No válido")
+}
+
+
