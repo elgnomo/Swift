427b1ce62312dd0b64d838e668ba6e9a32e18ccd
elGnomo <efrainantonio@ideashappy.com>
2018-10-30 03:15:06 -0600

    Accesos

---
+++ 27_Control_de_acceso.playground/Contents.swift
@@ -0,0 +1,11 @@
+/*
+     En swift tenemos 5 niveles de acceso:
+ 
+     Nivel de acceso Open: Cualquier puede tener acceso
+     Nivel de acceso Public: Tiene el mismo nivel de acceso que Open, pero la restricción que tiene es que solo puede hacerlo dentro de su módulo.
+     Nivel de acceso Internal: por ejemplo cuando creamos una función.
+     Nivel de acceso File-Private: acceso únicamente del archivo fuente donde fue declarado.
+     Nivel de acceso Private: acceso únicamente a la declaración y sus extensiones.
+ */
+
+var cursoSwift = Curso(nombre: "Curso de Swift", imagen:"platzi.com/swift.jpg")
---
+++ 27_Control_de_acceso.playground/Sources/Cursos.swift
@@ -0,0 +1,10 @@
+public class Curso {
+    public var nombre : String
+    public var imagen : String
+    
+    //init(nombre : String, imagen : String){
+    public init(nombre : String, imagen : String){
+        self.nombre = nombre
+        self.imagen = imagen
+    }
+}
