03090f724b69c3674aed1451710d930253c75dff
elGnomo <efrainantonio@ideashappy.com>
2018-10-30 20:29:45 -0600

    El evento touch

--- 29_practica_juego/gnomozzel/gnomozzel/ViewController.swift
+++ 29_practica_juego/gnomozzel/gnomozzel/ViewController.swift
@@ -8,17 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var board: UIView!//puente propiedades
+    @IBOutlet weak var board: UIView!
     var tileWidth : CGFloat = 0.0
     var tileCenterX : CGFloat = 0.0
     var tileCenterY : CGFloat = 0.0
     
-    var tileArray : NSMutableArray = []//label
-    var tileCenterArray : NSMutableArray = []//posicion
+    var tileArray : NSMutableArray = []
+    var tileCenterArray : NSMutableArray = []
     
-    @IBAction func btnRandom(_ sender: Any) {//acciones comportamiento
+    
+    @IBAction func btnReset(_ sender: Any) {
         randomTiles()
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -27,7 +29,7 @@ class ViewController: UIViewController {
     }
     
     func makeTiles(){
-        self.tileArray = [] //inicializar en ceros vacios
+        self.tileArray = []
         self.tileCenterArray = []
         let boardWidth = self.board.frame.width // ancho del tablero
         self.tileWidth = boardWidth / 4   // ancho de cada cuadro para que puedan entrar 4 cuadros por cada fila
@@ -45,11 +47,12 @@ class ViewController: UIViewController {
                 let currentCenter : CGPoint = CGPoint(x: self.tileCenterX, y: self.tileCenterY)
                 tile.center = currentCenter
                 tile.originCenter = currentCenter
+                tile.isUserInteractionEnabled = true
                 
                 self.tileCenterArray.add(currentCenter)
                 tile.backgroundColor = UIColor.red
                 tile.text = String(tileNumber)
-                tile.textAlignment = NSTextAlignment.center//recibe el valor de la posicion
+                tile.textAlignment = NSTextAlignment.center
                 tile.font = UIFont.systemFont(ofSize: 24)
                 tileNumber += 1
                 
@@ -70,18 +73,24 @@ class ViewController: UIViewController {
     func randomTiles(){
         let tempTileCenterArray : NSMutableArray = self.tileCenterArray.mutableCopy() as! NSMutableArray
         for anyTile in self.tileArray{
-            let randomIndex : Int = Int(arc4random()) % self.tileCenterArray.count// obtenemos aleatoriamente un centro de nuestro array
-            //arc4random devolver un numero aleatorio
-            let randomCenter : CGPoint = self.tileCenterArray[randomIndex] as! CGPoint
+            let randomIndex : Int = Int(arc4random()) % tempTileCenterArray.count// obtenemos aleatoriamente un centro de nuestro array
+            let randomCenter : CGPoint = tempTileCenterArray[randomIndex] as! CGPoint
             
             (anyTile as! CustomLabel).center = randomCenter
-            self.tileCenterArray.removeObject(at: randomIndex)
+            tempTileCenterArray.removeObject(at: randomIndex)
             
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let currentTouch : UITouch = touches.first!
+        if (self.tileArray.contains(currentTouch.view as Any)){
+            currentTouch.view?.alpha = 0
         }
     }
 }
 
-class CustomLabel : UILabel{//Herencia de la clasa UILABEL CLASES siempre ne mayusculas
+class CustomLabel : UILabel{
     var originCenter : CGPoint = CGPoint(x: 0, y: 0)
 }
 
